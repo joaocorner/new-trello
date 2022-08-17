f8b6a7ab3546674711d94593d54738b8ff73c217
joaocorner <joaoeduardocorner@gmail.com>
2022-08-17 12:09:57 -0300

    changing card type

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,9 +1,14 @@
 import Card from "react-bootstrap/Card";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Output = example => {
   return (
-    <Card>
-      <Card.Body>Example</Card.Body>
+    <Card style={{ width: '18rem' }}>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
     </Card>
   );
 };
