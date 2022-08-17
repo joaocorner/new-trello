e5066213c7cd87e6ec6bec4f10327c8d19187b6c
joaocorner <joaoeduardocorner@gmail.com>
2022-08-17 18:59:52 -0300

    changing array and using useState

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,24 +1,24 @@
 import Card from "react-bootstrap/Card";
 import "./card.css";
-
-const tasks = [
-  {
-    id: 1,
-    title: "Learn React",
-    text: "Exam will be on the 15th of May",
-    tag: "exam",
-    status: "To Do",
-  },
-  {
-    id: 2,
-    title: "Learn Node",
-    text: "Node is essential for the backend",
-    tag: "job",
-    status: "Doing",
-  },
-];
+import { useState } from "react";
 
 const Output = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Learn React",
+      text: "Exam will be on the 15th of May",
+      tag: "exam",
+      status: "To Do",
+    },
+    {
+      id: 2,
+      title: "Learn Node",
+      text: "Node is essential for the backend",
+      tag: "job",
+      status: "Doing",
+    },
+  ]);
   return (
     <div>
       {tasks.map((task) => (
