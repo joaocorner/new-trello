874aa4b7e1a5b346df27864dc72743c317ee55c5
joaocorner <joaoeduardocorner@gmail.com>
2022-08-17 15:53:21 -0300

    preparing layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import Input from "./components/Input";
-import Output from "./components/Output";
 
 function App() {
   return (
     <div className="App">
-      <Output />
+      <Input />
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,74 @@
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Output from "./Output";
 
 const Input = () => {
   return (
     <div>
-      <InputGroup className="mb-3">
-        <Form.Control
-          aria-label="Example text with button addon"
-          aria-describedby="basic-addon1"
-        />
-        <Button variant="outline-secondary" id="button-addon1">
-          Add
-        </Button>
-      </InputGroup>
+      <Container>
+        <Row>
+          <Col>To Do</Col>
+          <Col>Doing</Col>
+          <Col>Done</Col>
+        </Row>
+        <Row>
+          <Col>
+            <Output />
+          </Col>
+          <Col>
+            <Output />
+          </Col>
+          <Col>
+            <Output />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <InputGroup className="mb-3" style={{ width: "15rem" }}>
+              <InputGroup></InputGroup>
+              <Form.Control as="textarea" aria-label="With textarea" />
+            </InputGroup>
+          </Col>
+          <Col>
+            {" "}
+            <InputGroup className="mb-3" style={{ width: "15rem" }}>
+              <InputGroup></InputGroup>
+              <Form.Control as="textarea" aria-label="With textarea" />
+            </InputGroup>
+          </Col>
+          <Col>
+            {" "}
+            <InputGroup className="mb-3" style={{ width: "15rem" }}>
+              <InputGroup></InputGroup>
+              <Form.Control as="textarea" aria-label="With textarea" />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {" "}
+            <Button variant="primary" size="sm">
+              +{" "}
+            </Button>
+          </Col>
+          <Col>
+            {" "}
+            <Button variant="primary" size="sm">
+              +{" "}
+            </Button>
+          </Col>
+          <Col>
+            {" "}
+            <Button variant="primary" size="sm">
+              +{" "}
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
