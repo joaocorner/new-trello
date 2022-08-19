acf06bf02a50fb2a06c6f1929218b608f131d34b
joaocorner <joaoeduardocorner@gmail.com>
2022-08-19 00:12:47 -0300

    adding close button instead of double click

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,5 +1,6 @@
 import Card from "react-bootstrap/Card";
 import "./card.css";
+import { FaTimes } from "react-icons/fa";
 
 const Output = ({ task, onDelete, onChange }) => {
   return (
@@ -8,10 +9,11 @@ const Output = ({ task, onDelete, onChange }) => {
         key={task.id}
         className="bg-light cards"
         style={{ width: "18rem", height: "10rem" }}
-        onClick={() => onChange(task.id)}
-        onDoubleClick={() => onDelete(task.id)}
       >
-        <Card.Title className="title">{task.title}</Card.Title>
+        <FaTimes className="close" onClick={() => onDelete(task.id)} />
+        <Card.Title className="title" onClick={() => onChange(task.id)}>
+          {task.title}
+        </Card.Title>
         <Card.Subtitle className="tag">{task.tag}</Card.Subtitle>
         <Card.Text className="text">{task.text}</Card.Text>
       </Card>
