6c85c4ed28ba0fb7fcbbc5d4652219718a0cc95f
joaocorner <joaoeduardocorner@gmail.com>
2022-08-19 10:19:17 -0300

    inserting home page css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,14 @@
+import "./home.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome!</h1>
-      <p>To use this site, insert your 'To Do' list in the field and save. It'll be put in 'To Do' column. If you want to put it in 'Done', click in the title, or bring it back in 'To Do' by clicking again in the title. For erasing, click on the red X</p>
+    <div className="container-home">
+      <a href="/todolist" className="link">
+        <h1 className="title">To Do List!</h1>
+      </a>
+      <p className="howto">
+        To use this site, access clicking on the Title above. Insert your 'To Do' list in the field and save. It'll be put in 'To Do' column. If you want to put it in 'Done', click in the title, or bring it back in 'To Do' by clicking again in the title. For erasing, click on the red X
+      </p>
     </div>
   );
 }
