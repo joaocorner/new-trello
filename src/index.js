2fd67b1e00724f5ba72b98775e1cb296314e7053
joaocorner <joaoeduardocorner@gmail.com>
2022-08-22 18:46:09 -0300

    removing redux

--- src/index.js
+++ src/index.js
@@ -2,21 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import { Provider } from "react-redux";
-import store from "./store";
-
 import RoutesApp from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <RoutesApp />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <RoutesApp />
+  </React.StrictMode>
 );
 
 reportWebVitals();
