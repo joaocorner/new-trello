d66a325a16841b02d6ee5551c2e01debc7cd9f32
joaocorner <joaoeduardocorner@gmail.com>
2022-08-25 22:09:34 -0300

    updating home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <h1 className="title">To Do List!</h1>
       </a>
       <p className="howto">
-        To use this site, access clicking on the Title above. Insert your 'To Do' list in the field and save. It'll be put in 'To Do' column. If you want to put it in 'Done', click in the title, or bring it back in 'To Do' by clicking again in the title. For erasing, click on the red X
+        To use this site, access clicking on the Title above. Insert your 'To Do' list in the field and save. It'll be put in 'To Do' column. If you want to put it in 'Done', click in the title, or bring it back in 'To Do' by clicking again in the title. For erasing, click on the red X If you want to create a new list, write its name after the / in the URL. For example, if you want to create a list called 'Shopping', you should write new-trello.vercel.app/Shopping
       </p>
     </div>
   );
